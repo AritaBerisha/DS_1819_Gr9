a8a9732e4fc394001f5f4ccb0bf195a7db54ac27
boredalis <aurora.selimi@studentet.uni-pr.edu>
2019-06-16 17:30:25 +0200

    server-client

--- DS2/server.js
+++ DS2/server.js
@@ -14,34 +14,35 @@ server.on('error', (err) => {
     server.close();
 });
 
-server.on('message', (msg, rinfo) => {
+server.on('message', (msg,rinfo)=>{
     console.log(`server got message from ${rinfo.address}:${rinfo.port}`);
-    saltedHash = saltedSha1(SplitArray(msg.toString())[1], 'SUPER-S@LT!');
-    if (db.get("user").find({ username: SplitArray(msg.toString())[0] }).value()) {
-        server.send(Buffer.from("Username already Exists"), rinfo.port, rinfo.address);
-    } else {
-        db.get("user").push({
-            username: SplitArray(msg.toString())[0],
-            password: saltedHash,
-            id: SplitArray(msg.toString())[2],
-            AverageGrade: SplitArray(msg.toString())[3],
-            Faculty: SplitArray(msg.toString())[4]
-        }).write();
-        server.send(Buffer.from("Thank u"), rinfo.port, rinfo.address);
+    let el=msg.toString().split(',');
+    saltedHash = saltedSha1(el[2], 'SUPER-S@LT!');
+    if(el[0]==='r'){
+        if (db.get("user").find({ username: el[1] }).value()) {
+            server.send("Account already exists", rinfo.port, rinfo.address);
+        } else {
+            db.get("user").push({
+                username: el[1],
+                password: saltedHash,
+                ID:el[3],
+                GPA:el[4],
+                Faculty: el[5]
+            }).write();
+            server.send(["You're now registered!",(Object.values(db.get("user").find({ username: el[1] }).value())).toString()], rinfo.port, rinfo.address);
+        }
+    }else if(el[0]==='l'){
+        if(db.get("user").find({ username: el[1] }).value() && 
+            db.get("user").find({ password: saltedHash }).value()){
+            server.send(["You're now logged in!",(Object.values(db.get("user").find({ username: el[1] }).value())).toString()], rinfo.port, rinfo.address);
+        }else{
+            server.send("This account doesn't exist",rinfo.port, rinfo.address);
+        }
     }
-
-
 });
-
 server.on('listening', () => {
     const address = server.address();
     console.log(`server listening ${address.address}:${address.port}`);
 });
 
-server.bind(41237);
-
-function SplitArray(array) {
-
-    let array2 = array.split(" ");
-    return array2;
-}
+server.bind(8080);
